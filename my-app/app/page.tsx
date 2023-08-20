bfe91c3bf9886d83479c4afbf62ef6f0b792401b
Rodz-Vskmann <augustozuanazzi03@gmail.com>
2023-08-20 13:00:22 -0300

    atualizações gerais

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -104,67 +104,35 @@ export default function Home() {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
+              <path/>
             </svg>
           </div>
           <div className="featured_info">
-            <span>Built for developers </span>
+            <span></span>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              ratione facilis animi voluptas exercitationem molestiae.
             </p>
           </div>
         </div>
         <div className="grid-col-item">
           <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
-              />
+            <svg>
+              <path/>
             </svg>
           </div>
           <div className="featured_info">
-            <span>Designed to be modern</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsum
-              esse corrupti. Quo, labore debitis!
-            </p>
+            <span></span>
+            <p></p>
           </div>
         </div>
         <div className="grid-col-item">
           <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-              />
+            <svg>
+              <path/>
             </svg>
           </div>
           <div className="featured_info">
-            <span>Documentation for everything</span>
+            <span></span>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-              nostrum voluptate totam ipsa corrupti vero!
             </p>
           </div>
         </div>
