a3e5456235905462f639ad0b9ab21594f11a1371
Rodz-Vskmann <augustozuanazzi03@gmail.com>
2023-08-29 17:39:11 -0300

    detalhes ao editor de texto

--- my-app/app/mynotes/page.tsx
+++ my-app/app/mynotes/page.tsx
@@ -3,34 +3,34 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './styles.css';
-import { Html } from 'next/document';
+
 
 const modules = {
   toolbar: [
-    [{ 'font': [] }, { 'size': ['small', false, 'large', 'huge'] }],  // fonte e tamanho de fonte
-    ['bold', 'italic', 'underline', 'strike'], // estilos
-    [{ 'color': [] }, { 'background': [] }],  // cores de texto e fundo
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],  // listas
-    [{ 'align': [] }],  // alinhamento
-    [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block'], // cabeçalhos e blocos de código
-    ['link', 'image', 'video']  // links, imagens e vídeos
+    [{ 'font': [] }, { 'size': ['small', false, 'large', 'huge'] }],  
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'align': [] }],
+    [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block'],
+    ['link', 'image', 'video']
   ]
 };
 
-// mynotes/page.tsx
 const Page = () => {
   const [editorHtml, setEditorHtml] = useState('');
 
-  function handleChange() {
-    setEditorHtml();
-  }
+  const handleChange = () => {
+    setEditorHtml(Html);
+  };
 
   return (
-    <div>
-      <h1>Papyrus.</h1>
-      <ReactQuill value={editorHtml} onChange={handleChange} modules={modules} />
-    </div>
-  );
+   
+      <div className="my-editor-container"> {/* adicionada uma classe para estilização */}
+        <h1>Papyrus.</h1>
+        <ReactQuill value={editorHtml} onChange={handleChange} modules={modules} />
+      </div>
+    );
 };
 
 export default Page;
